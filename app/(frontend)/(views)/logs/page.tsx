6a6b6fdb063283aecfdbd28231cfd8fcc947075d
gilberto <gilbertowuwu0@gmail.com>
2026-06-10 08:11:05 -0400

    aprendizaje: hay componentes que se convierten en contenido estatico y por eso no vuelve a actualizarse

--- app/(frontend)/(views)/logs/page.tsx
+++ app/(frontend)/(views)/logs/page.tsx
@@ -1,6 +1,8 @@
 import Logs from "./logs";
 import BlueprintsModel from "@models/blueprints";
 
+export const dynamic = "force-dynamic";
+
 export default async function LogsView() {
 
   const blueprints = await BlueprintsModel.getAll();
